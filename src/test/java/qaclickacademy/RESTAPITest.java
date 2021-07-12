8334a19079c55952d3d349549c4859245bb91d1c
anand <anand11049@gmail.com>
2021-07-12 11:16:19 +0530

    first commit

---
+++ src/test/java/qaclickacademy/RESTAPITest.java
@@ -0,0 +1,15 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+	
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+}
+	
+
+
